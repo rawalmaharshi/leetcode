2e2c92d78d84b57f53ab15b5839880a4ccb7dd64
Maharshi Rawal <rawalmaharshi@gmail.com>
2022-05-04 20:18:43 -0400

    Update answer for 118. Pascal's Triangle

--- 118/Solution.java
+++ 118/Solution.java
@@ -1,20 +1,53 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> answer = new ArrayList<>(numRows);
-        for (int i = 0; i < numRows; i++) {
-            List<Integer> smallAns = new ArrayList<>(i + 1);
-            List<Integer> prevList = null;
-            if (i > 1)  prevList = answer.get(i - 1);
-            for (int j = 0; j < (i + 1); j++) {
-                if (j == 0 || j == i) {
-                    smallAns.add(1);
-                } else {
-                    int element = prevList.get(j - 1) + prevList.get(j);
-                    smallAns.add(element);
-                }
-            }
-            answer.add(smallAns);
+        // return iterativeHelper(numRows);
+        return recursiveHelper(numRows);
+    }
+    
+    public List<List<Integer>> recursiveHelper(int numRows) {
+        //base case
+        if (numRows == 1) {
+            List<Integer> smallAns = Arrays.asList(1);
+            List<List<Integer>> ans = new ArrayList<>();
+            ans.add(smallAns);
+            return ans;
         }
+        
+        List<List<Integer>> smallAns = recursiveHelper(numRows - 1);
+        List<List<Integer>> ans = new ArrayList<>();
+        //add prev smallAns to ans
+        ans.addAll(smallAns);
+        
+        List<Integer> currListToAdd = new ArrayList<>();
+        List<Integer> prevList = smallAns.get(numRows - 2);
+        currListToAdd.add(1);
+        for (int i = 1; i < prevList.size(); i++) {
+            currListToAdd.add(prevList.get(i - 1) + prevList.get(i));
+        }
+        currListToAdd.add(1);
+        ans.add(currListToAdd);
+        
+        return ans;
+    }
+    
+    public List<List<Integer>> iterativeHelper(int numRows) {
+        List<List<Integer>> answer = new ArrayList<>();
+        List<Integer> rowOneList = Arrays.asList(1);
+        answer.add(rowOneList);
+        for (int i = 1; i < numRows; i++) {
+            List<Integer> prevRow = answer.get(i - 1);
+            List<Integer> currRowList = new ArrayList<>();
+            //1st and last element are always null
+            currRowList.add(1);
+            for (int j = 1; j < prevRow.size(); j++) {
+                currRowList.add(prevRow.get(j - 1) + prevRow.get(j));
+            }
+            currRowList.add(1);
+            
+            //add current list to answer;
+            answer.add(currRowList);
+        }
+        
         return answer;
     }
 }
