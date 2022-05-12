adefb48bc79b0bd2265229fd69bf922025c4346c
Maharshi Rawal <rawalmaharshi@gmail.com>
2022-05-12 00:31:59 -0400

    add dp solution for 70. Climbing stairs

--- 70/Solution.java
+++ 70/Solution.java
@@ -5,12 +5,29 @@ class Solution {
         if (n <= 2) {
             return n;
         }
-        
-        int[] stairs = new int[n + 1];
-        stairs[1] = 1;
-        stairs[2] = 2;
+        // return memo(n);
+        return dp(n);
+    }
+    
+    public int dp(int n) {
+        int[] dp = new int[n + 1];
+        //base cases
+        dp[1] = 1; dp[2] = 2;
         for (int i = 3; i < n + 1; i++) {
-            stairs[i] = -1;
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        
+        return dp[n];
+    }
+    
+    public int memo(int n) {
+        int[] stairs = new int[n + 1];
+        for (int i = 0; i < n + 1; i++) {
+            if (i == 1 || i == 2) {
+                stairs[i] = i;
+            } else {
+                stairs[i] = -1;
+            }
         }
         
         return memoizationHelper(n, stairs);
@@ -18,11 +35,12 @@ class Solution {
     
     public int memoizationHelper(int n, int[] stairs) {
         //base case
-        if (n == 2) {
+        if (n <= 2) {
+            stairs[n] = n;
             return n;
         }
         
-        if (stairs[n - 1] == -1 || stairs[n - 2] == -1) {
+        if (stairs[n] == -1) {
             memoizationHelper(n - 1, stairs);
         }
         
