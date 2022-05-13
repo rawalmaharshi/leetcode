b149ac5524969a130962e493ddd3eb08a9afd979
Maharshi Rawal <rawalmaharshi@gmail.com>
2022-05-12 21:59:03 -0400

    198. House Robber

---
+++ 198/Solution.java
@@ -0,0 +1,31 @@
+class Solution {
+    private Map<Integer, Integer> memo = new HashMap<>();
+    private int[] nums;
+    public int rob(int[] nums) {
+        return usingMemoization(nums, nums.length - 1);
+    }
+    
+    public int usingMemoization(int[] nums, int n) {
+        this.nums = nums;
+        return memoHelper(n);
+    }
+    
+    public int memoHelper(int n) {
+        //base case(s)
+        if (n == 0) {
+            return nums[0];
+        }
+        
+        //Can either rob from house at index 0 or 1; will rob whereever max money
+        if (n == 1) {
+            return Math.max(nums[0], nums[1]);
+        }
+    
+        //Max of previous house or (current house + house at currentIndex - 2)
+        if (!memo.containsKey(n)) {
+            memo.put(n, Math.max(memoHelper(n - 1), memoHelper(n - 2) + nums[n]));
+        }
+        
+        return memo.get(n);
+    }
+}
