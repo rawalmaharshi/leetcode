8bd20ae80858ebd59a6b401cd285d7a3169c8d8b
Maharshi Rawal <rawalmaharshi@gmail.com>
2022-05-12 22:03:35 -0400

    198. House Robber - add dp solution

--- 198/Solution.java
+++ 198/Solution.java
@@ -2,7 +2,25 @@ class Solution {
     private Map<Integer, Integer> memo = new HashMap<>();
     private int[] nums;
     public int rob(int[] nums) {
-        return usingMemoization(nums, nums.length - 1);
+        // return usingMemoization(nums, nums.length - 1);
+        return usingDP(nums);
+    }
+    
+    public int usingDP(int[] nums) {
+        int length = nums.length;
+        //edge case
+        if (length == 1) {
+            return nums[0];
+        }
+        
+        int[] dp = new int[length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < length; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+        }
+        
+        return dp[length - 1];
     }
     
     public int usingMemoization(int[] nums, int n) {
