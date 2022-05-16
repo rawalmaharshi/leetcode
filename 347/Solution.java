ea5b4b14d13a7984a6a6d11ba007d4bb8807e11c
Maharshi Rawal <rawalmaharshi@gmail.com>
2022-05-16 17:20:40 -0400

    update 347. Top k frequent elements.

--- 347/Solution.java
+++ 347/Solution.java
@@ -1,33 +1,44 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        if (nums.length == 0 || nums == null) {
-            return new int[] {};
-        }
-        
-        //edge case
-        if (nums.length == k) {
-            return nums;
-        }
-        
         Map<Integer, Integer> countMap = new HashMap<>();
         for (int num : nums) {
             countMap.put(num, countMap.getOrDefault(num, 0) + 1);
         }
         
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> countMap.get(a) - countMap.get(b));
-        for (int key : countMap.keySet()) {
-            pq.offer(key);
-            if (pq.size() > k) {
-                pq.poll();
+        // return usingMinPQ(k, countMap);
+        return usingMaxPQ(k, countMap);
+    }
+    
+    public int[] usingMinPQ(int k, Map<Integer, Integer> countMap) {
+        PriorityQueue<Integer> minPQ = new PriorityQueue<>((a, b) -> countMap.get(a) - countMap.get(b));
+        for (int num : countMap.keySet()) {
+            minPQ.offer(num);
+            if (minPQ.size() > k) {
+                minPQ.poll();
             }
         }
         
-        int[] ans = new int[k];
+        int[] answer = new int[k];
         int ansIndex = 0;
-        while (!pq.isEmpty()) {
-            ans[ansIndex++] = pq.poll();
+        while (!minPQ.isEmpty()) {
+            answer[ansIndex++] = minPQ.poll();
         }
         
-        return ans;
+        return answer;
+    }
+    
+    public int[] usingMaxPQ(int k, Map<Integer, Integer> countMap) {
+        PriorityQueue<Integer> maxPQ = new PriorityQueue<>((a, b) -> countMap.get(b) - countMap.get(a));
+        for (int num : countMap.keySet()) {
+            maxPQ.offer(num);
+        }
+        
+        int[] answer = new int[k];
+        int ansIndex = 0;
+        while (k-- > 0) {
+            answer[ansIndex++] = maxPQ.poll();
+        }
+        
+        return answer;
     }
 }
