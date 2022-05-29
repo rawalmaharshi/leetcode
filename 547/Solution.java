5a050d4c5b7a747a42c3cf99c275e9877a3623ee
Maharshi Rawal <rawalmaharshi@gmail.com>
2022-05-29 19:06:25 -0400

    Add union find solution to number of provinces

--- 547/Solution.java
+++ 547/Solution.java
@@ -53,3 +53,83 @@ class Solution {
         return;
     }
 }
+
+/**
+------------------ SOLUTION USING UNION FIND ------------------
+ */
+
+class Solution2 {
+    public int findCircleNum(int[][] isConnected) {
+        if (isConnected == null || isConnected.length == 0) {
+            return 0;
+        }
+        
+        UnionFind uf = new UnionFind(isConnected.length);
+        for (int i = 0; i < isConnected.length; i++) {
+            for (int j = 0; j < isConnected[i].length; j++) {
+                if (i != j && isConnected[i][j] == 1) {
+                    uf.union(i, j);
+                }
+            }
+        }
+        
+        return uf.getProvinceCount();
+    }
+}
+
+class UnionFind {
+    private int[] root;
+    private int[] rank;
+    private int count;
+    
+    public UnionFind(int size) {
+        this.root = new int[size];
+        this.rank = new int[size];
+        
+        //All the provinces are disconnected in the beginning
+        this.count = size;
+        
+        //Initialize root and rank array
+        for(int i = 0; i < size; i++) {
+            root[i] = i;
+            rank[i] = 1;
+        }
+    }
+    
+    //Find using path compression optimization
+    public int find(int x) {
+        if (x == root[x]) {
+            return x;
+        }
+        
+        return root[x] = find(root[x]);
+    }
+    
+    //Union using union by rank optimization
+    public void union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        
+        if (rootX != rootY) {
+            if (rank[rootX] > rank[rootY]) {
+            root[rootY] = rootX;
+            } else if (rank[rootX] < rank[rootY]) {
+                root[rootX] = rootY;
+            } else {
+                //Both are equal set y's root as x; increase rootx's rank
+                root[rootY] = rootX;
+                rank[rootX] += 1;
+            }
+            //Decrease the number of province since now two independent provinces are joined (union)
+            this.count--;   
+        }
+    }
+    
+    public boolean isConnected(int x, int y) {
+        return find(x) == find(y);
+    }
+    
+    public int getProvinceCount() {
+        return this.count;
+    }
+}
