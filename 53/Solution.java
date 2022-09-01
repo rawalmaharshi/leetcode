ac284ab0f7757007bf97c4bb53f02b0060df48c4
Maharshi Rawal <rawalmaharshi@gmail.com>
2022-09-01 14:47:58 -0400

    53. Maximum Subarray - add divide and conquer solution

--- 53/Solution.java
+++ 53/Solution.java
@@ -1,12 +1,45 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int sum = nums[0];
-        int max = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            sum= Math.max(sum + nums[i], nums[i]);
-            max = Math.max(max, sum);
+        return divideAndConquer(nums, 0, nums.length - 1);
+        // return optimizedDP(nums);
+    }
+    
+    public int optimizedDP(int[] nums) {
+        int maxSum = nums[0], currSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            currSum = Math.max(currSum + nums[i], nums[i]);
+            maxSum = Math.max(maxSum, currSum);
         }
         
-        return max;
+        return maxSum;
+    }
+    
+    //Unintuitive approach - just for reference
+    public int divideAndConquer(int[] nums, int left, int right) {
+        //base case
+        if (left > right) {
+            return Integer.MIN_VALUE;
+        }
+        
+        int mid = left + (right - left)/2;
+        
+        int sum = 0, leftBestSum = 0, rightBestSum = 0;
+        for (int i = mid - 1; i >= left; i--) {
+            sum += nums[i];
+            leftBestSum = Math.max(leftBestSum, sum);
+        }
+        
+        sum = 0;
+        for (int i = mid + 1; i <= right; i++) {
+            sum += nums[i];
+            rightBestSum = Math.max(rightBestSum, sum);
+        }
+        
+        int combinedSum = leftBestSum + nums[mid] + rightBestSum;
+        
+        int leftSum = divideAndConquer(nums, left, mid - 1);
+        int rightSum = divideAndConquer(nums, mid + 1, right);
+        
+        return Math.max(combinedSum, Math.max(leftSum, rightSum));
     }
 }
