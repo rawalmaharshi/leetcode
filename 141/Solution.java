9d7492a7795c0fe3bbf1530f521459b831300754
Maharshi Rawal <rawalmaharshi@gmail.com>
2022-09-05 19:12:56 -0400

    141. Add set based solution for linked list cycle

--- 141/Solution.java
+++ 141/Solution.java
@@ -11,12 +11,17 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
+        // return usingSet(head);
+        return usingSlowAndFastPointer(head);
+    }
+    
+    public boolean usingSlowAndFastPointer(ListNode head) {
+        //edge case
         if (head == null) {
             return false;
         }
-
-        ListNode slow = head;
-        ListNode fast = head.next;
+        
+        ListNode slow = head, fast = head.next;
         while (slow != fast) {
             if (fast == null || fast.next == null) {
                 return false;
@@ -24,6 +29,20 @@ public class Solution {
             slow = slow.next;
             fast = fast.next.next;
         }
+        
         return true;
+    }
+    
+    public boolean usingSet(ListNode head) {
+        Set<ListNode> set = new HashSet<>();
+        while (head != null) {
+            if (set.contains(head)) {
+                return true;
+            }
+            set.add(head);
+            head = head.next;
+        }
+        
+        return false;
     }
 }
