e5e1d4464ee0b179b7c046f911813d70fd0ec807
Maharshi Rawal <rawalmaharshi@gmail.com>
2022-09-08 22:02:42 -0400

    15. 3Sum - Add hashset based solution

--- 15/Solution.java
+++ 15/Solution.java
@@ -1,10 +1,38 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         // return bruteForce(nums);
-        return optimized(nums);
+        // return usingTwoPointerTLE(nums);
+        return usingTwoPointerHashset(nums);
+    }
+
+    public List<List<Integer>> usingTwoPointerHashset(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> answer = new ArrayList<>();
+        
+        for (int i = 0; i < nums.length && nums[i] <= 0; i++) {
+            if (i == 0 || nums[i - 1] != nums[i]) {
+                twoSum(nums, i, answer);
+            }
+        }
+        
+        return answer;
     }
     
-    public List<List<Integer>> optimized(int[] nums) {
+    private void twoSum(int[] nums, int i, List<List<Integer>> answer) {
+        Set<Integer> set = new HashSet<>();
+        for (int j = i + 1; j < nums.length; j++) {
+            int complement = -nums[i] - nums[j];
+            if (set.contains(complement)) {
+                answer.add(Arrays.asList(nums[i], nums[j], complement));
+                while (j + 1 < nums.length && nums[j] == nums[j + 1]) {
+                ++j;
+                }
+            }
+            set.add(nums[j]);
+        }
+    }
+    
+    public List<List<Integer>> usingTwoPointerTLE(int[] nums) {
         List<List<Integer>> answer = new ArrayList<>();
         Arrays.sort(nums);
         for (int i = 0; i < nums.length; i++) {
