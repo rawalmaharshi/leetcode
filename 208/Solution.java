771fece444127d563649f6b56be8413724f1ed53
Maharshi Rawal <rawalmaharshi@gmail.com>
2022-09-14 20:10:50 -0400

    208. Implement Trie (Prefix Tree)

---
+++ 208/Solution.java
@@ -0,0 +1,81 @@
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
+
+class TrieNode {
+    private TrieNode[] links;
+    private final int R = 26;
+    private boolean isEnd;
+
+    public TrieNode() {
+       links = new TrieNode[26]; 
+    }
+
+    public boolean containsKey(char c) {
+        return links[c - 'a'] != null;
+    }
+
+    public TrieNode get(char c) {
+        return links[c - 'a'];
+    }
+
+    public void put(char c, TrieNode node) {
+        links[c - 'a'] = node;
+    }
+
+    public void setEnd() {
+        isEnd = true;
+    }
+
+    public boolean isEnd() {
+        return isEnd;
+    }
+}
+
+class Trie {
+    private TrieNode root;
+    
+    public Trie() {
+        root = new TrieNode();
+    }
+    
+    public void insert(String word) {
+        TrieNode node = root;
+        for (int i = 0; i < word.length(); i++) {
+            char curr = word.charAt(i);
+            if (!node.containsKey(curr)) {
+                node.put(curr, new TrieNode());
+            }
+            node = node.get(curr);
+        }
+        
+        node.setEnd();
+    }
+    
+    public boolean search(String word) {
+        TrieNode node = searchPrefix(word);
+        return node != null && node.isEnd();
+    }
+    
+    public boolean startsWith(String prefix) {
+        TrieNode node = searchPrefix(prefix);
+        return node != null;
+    }
+    
+    private TrieNode searchPrefix(String word) {
+        TrieNode node = root;
+        for (int i = 0; i < word.length(); i++) {
+            char curr = word.charAt(i);
+            if (node.containsKey(curr)) {
+                node = node.get(curr);
+            } else {
+                return null;
+            }
+        }
+        return node;
+    }
+}
