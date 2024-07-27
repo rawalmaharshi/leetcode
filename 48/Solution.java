af2a50c91b609edc8dbd525067dbdc1fb9984de5
Maharshi Rawal <rawalmaharshi@gmail.com>
2024-07-27 13:07:33 -0400

    Update solution for 48. Rotate Image

--- 48/Solution.java
+++ 48/Solution.java
@@ -1,5 +1,10 @@
 class Solution {
     public void rotate(int[][] matrix) {
+        usingRotation(matrix);
+        // usingTransposeAndReflect(matrix);
+    }
+
+    public void usingRotation(int[][] matrix) {
         int n = matrix.length;
         for (int i = 0; i < (n + 1)/2; i++) {
             for (int j = 0; j < n/2; j++) {
@@ -8,6 +13,32 @@ class Solution {
                 matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 -i];
                 matrix[j][n - 1 - i] = matrix[i][j];
                 matrix[i][j] = temp;
+            }
+        }
+    }
+
+    public void usingTransposeAndReflect(int[][] matrix) {
+        transpose(matrix);
+        reflect(matrix);
+    }
+
+    public void transpose(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i + 1; j < matrix[0].length; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
+
+    public void reflect(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n/2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - j - 1];
+                matrix[i][n - j - 1] = temp;
             }
         }
     }
